c660a6ebbbd4e1da95f7b900fea9e417f3f30755
juliapolbach <julia@alternaenergetica.com>
2022-08-18 18:18:41 +0200

    ✨ Add a PullRequest mysql repository class to manage interactions with mysql DB

--- lib/pullRequest/infrastructure/pullRequest.mysql.repository.ts
+++ lib/pullRequest/infrastructure/pullRequest.mysql.repository.ts
@@ -1,35 +1,57 @@
 import { injectable } from 'tsyringe'
-import { MySQLDatasource } from '../../core'
-import { PullRequest } from '../../core/wrappers/types'
+import { Helper, MySQLDatasource } from '../../core'
+import { PullRequestRepository } from '../domain/pullRequest.repository'
+import { PullRequest } from '../domain/models/pullRequestTypes'
+import { PullRequestIdentifier } from '../../core/wrappers/types'
 
 @injectable()
-export class PullRequestMysqlRepository {
+export class PullRequestMysqlRepository implements PullRequestRepository {
   constructor () {}
 
-  async getTrackedPullRequestList (repositoryName: string): Promise<PullRequest[]> {
+  async getTrackedPullRequestList (): Promise<PullRequest[]> {
     const db = new MySQLDatasource()
+    const pullRequestList: PullRequest[] = []
 
     const [rows] = await db.pool.query(
       `SELECT r.*, pr.* from repository r
-           INNER JOIN pull_request pr on pr.repository = r.id
-           WHERE r.name = ${repositoryName}`)
+           INNER JOIN pull_request pr on pr.repository = r.id`)
 
     await db.pool.end()
 
-    const result = JSON.parse(JSON.stringify(rows))
+    const rawPullRequestList = JSON.parse(JSON.stringify(rows))
 
-    return result.map((pullRequest) => {
-      return {
-        id: pullRequest.id,
-        repository: {
-          name: pullRequest.name
-        },
-        title: pullRequest.title,
-        description: '',
-        isMergeable: null,
-        createdAt: pullRequest.creation_date,
-        status: pullRequest.status
-      }
-    })
+    for (const rawPullRequest of rawPullRequestList) {
+      pullRequestList.push(Helper.mapDatabasePullRequest(rawPullRequest))
+    }
+    return pullRequestList
+  }
+
+  async getTrackedPullRequestById (id: PullRequestIdentifier): Promise<PullRequest> {
+    const db = new MySQLDatasource()
+
+    const [rows] = await db.pool.query(
+      `SELECT * from pull_request pr
+           inner join repository r on r.id = pr.repository
+           WHERE r.name = ? AND pr.pull_request_number = ?`, [id.repoName, id.pullRequestId])
+
+    await db.pool.end()
+
+    const rawPullRequest = JSON.parse(JSON.stringify(rows))
+
+    return Helper.mapDatabasePullRequest(rawPullRequest[0])
+  }
+
+  async updatePullRequest (id: PullRequestIdentifier, pullRequest: PullRequest): Promise<void> {
+    const db = new MySQLDatasource()
+
+    await db.pool.query(
+      `UPDATE pull_request pr
+           INNER JOIN repository r on r.id = pr.repository
+           SET pr.title=?, pr.status=?
+           WHERE r.name=? AND pr.pull_request_number=?;`,
+      [pullRequest.title, pullRequest.status, id.repoName, id.pullRequestId]
+    )
+
+    await db.pool.end()
   }
 }
