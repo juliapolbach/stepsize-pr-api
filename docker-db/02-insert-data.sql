b37185d12a4c7e89f0759beb8689fe33fc27b234
juliapolbach <julia@alternaenergetica.com>
2022-08-18 18:24:26 +0200

    🗃️️ Add new inserts to Docker DB

--- docker-db/02-insert-data.sql
+++ docker-db/02-insert-data.sql
@@ -11,7 +11,13 @@ VALUES(2, 'repository-2', 'bitbucket', '2022-08-13 12:15:32');
 
 INSERT INTO stepsize.pull_request
 (code_hosting_provider, pull_request_number, repository, title, status, creation_date, last_updated)
-VALUES('bitbucket', 1, 2, 'Fix: Add some config files', 'open', '2022-08-17 14:33:14', '2022-08-17 18:30:22');
+VALUES('bitbucket', 1, 2, 'Fix: Add some config files', 'open', '2022-08-17 14:33:14', NULL);
 INSERT INTO stepsize.pull_request
 (code_hosting_provider, pull_request_number, repository, title, status, creation_date, last_updated)
 VALUES('bitbucket', 2, 2, 'Feature: Create new track endpoint', 'merged', '2022-08-17 14:36:38', NULL);
+INSERT INTO stepsize.pull_request
+(code_hosting_provider, pull_request_number, repository, title, status, creation_date, last_updated)
+VALUES('github', 5, 1, '✨ Add new cool feature', 'open', '2022-08-18 08:38:46', NULL);
+INSERT INTO stepsize.pull_request
+(code_hosting_provider, pull_request_number, repository, title, status, creation_date, last_updated)
+VALUES('github', 1, 1, 'Add info to README.md', 'merged', '2022-08-15 18:32:55', '2022-08-18 08:39:54');
